178933eca5fac82e96ff1e0fbe8a70a789d73a1a
Matei441 <mateitudose441@gmail.com>
2026-04-21 00:06:45 +0300

    Add HTML pages for home, login and register

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 from extensions import engine, Base
 from routes.auth import router as auth_router
 
@@ -7,3 +10,20 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(auth_router)
+
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get('/', response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse('home.html', {'request': request})
+
+
+@app.get('/login', response_class=HTMLResponse)
+def login_page(request: Request):
+    return templates.TemplateResponse('login.html', {'request': request})
+
+
+@app.get('/register', response_class=HTMLResponse)
+def register_page(request: Request):
+    return templates.TemplateResponse('register.html', {'request': request})
